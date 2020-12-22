cde84e3aeddfd233735270afdb43ae8fbb491209
Addepalli N M PavanKalyan <57014239+pavankalyan0424@users.noreply.github.com>
2020-12-22 13:33:47 +0530

    Home page refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_shops/pages/products_overview.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,30 +15,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: ProductOverViewPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Shops',
-        ),
-      ),
-      body: Center(
-        child: Text(
-          'Shops',
-        ),
-      ),
-    );
-  }
-}
